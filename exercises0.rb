dfd8d2ed8c68f708520afea8fc5639281ab8d711
Mark <markjconlon@gmail.com>
2017-03-28 19:38:47 -0400

    Step 0 complete

---
+++ exercises0.rb
@@ -0,0 +1,18 @@
+fav_colours= ["yellow", "red", "blue"]
+ages = [27, 32, 23]
+flips = ["heads", "tails", "tails"]
+performers = ["Skrillex", "Above and Beyond", "Linkin Park"]
+colours = [ :yellow, :red, :blue]
+
+definitions = { selfiecide: "when a person dies while trying to take a selfie in a dangerous area",
+                wealth_care: "excellent health care for the wealthy",
+                nmh: "Nodding My Head"}
+movie_names = { tron: 2010,
+                thecovenant: 2006,
+                interstellar: 2014}
+cities = { toronto: 2 615 000,
+            montreal: 1 650 000,
+            ottawa: 883 391}
+friends_ages = {mark: 27,
+                lauren: 23,
+                ben: 32}
