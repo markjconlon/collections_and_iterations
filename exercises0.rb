d1a19fa0ebbc78704dfa5abf7cf0e1f678ef3fe9
Mark <markjconlon@gmail.com>
2017-03-28 21:51:34 -0400

    exercise 4 complete

--- exercises0.rb
+++ exercises0.rb
@@ -17,39 +17,58 @@ friends_ages = {mark: 27,
                 lauren: 23,
                 ben: 32}
 
-fav_colours.each do |color|
-  puts color
-end
-
-print fav_colours[0] + "\n"
-
-puts (ages.sort)
-
-ages << 0
-
-puts (movie_names[:tron])
+# fav_colours.each do |color|
+#   puts color
+# end
+#
+# print fav_colours[0] + "\n"
+#
+# puts (ages.sort)
+#
+# ages << 0
+#
+# puts (movie_names[:tron])
 
 #Exercise 2
-puts fav_colours[-1]
-
-cities[:vancouver] = 630000
-
-flips = flips.reverse
-
-puts cities[:toronto]
-
-performers.each do |artist|
-  puts "I think #{artist} is great."
-end
+# puts fav_colours[-1]
+#
+# cities[:vancouver] = 630000
+#
+# flips = flips.reverse
+#
+# puts cities[:toronto]
+#
+# performers.each do |artist|
+#   puts "I think #{artist} is great."
+# end
 
 #Exercise 3
-puts performers[0] + " " + performers[1]
+# puts performers[0] + " " + performers[1]
+#
+# movie_names.each do |title, year|
+#   puts "#{title} came out in #{year}."
+# end
+#
+# print (ages.sort.reverse)
+#
+# movie_names.store("Beauty and the Beast", [1991, 2017])
+# puts movie_names
 
-movie_names.each do |title, year|
-  puts "#{title} came out in #{year}."
+#Exercise 4
+
+ages.each do |age|
+  if age < 30
+    puts age
+  end
 end
 
-print (ages.sort.reverse)
+puts (ages.max)
 
-movie_names.store("Beauty and the Beast", [1991, 2017])
-puts movie_names
+flips.count("heads")
+
+performers.delete("Skrillex")
+
+cities.store(:toronto, 2)
+puts cities
+
+#Exercise 5
